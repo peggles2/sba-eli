8021668558e2ec30223da621c9bc29ac421098e2
Andrew Wagner <awagner@Andrews-MBP.fios-router.home>
2018-12-05 13:58:30 -0500

    SBAELIDEV-210 initial version of login form

--- front-end/src/components/Navigation/Navigation.js
+++ front-end/src/components/Navigation/Navigation.js
@@ -13,6 +13,42 @@ const Navigation = () => {
         <NavigationDisplay />
       </Grid>
     </Router>
+
+const NavigationDisplay = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Dashboard} />
+      <Route exact path="/signup" component={SignUpForm} />
+      <Route exact path="/login" component={LoginForm} />
+      <Route exact path="/learning_paths" component={LearningPaths} />
+      <Route exact path="/learning_paths/:id" component={LearningPath} />
+      <Route
+        exact
+        path="/learning_paths/:course_id/learning_objectives/"
+        component={LearningObjectives}
+      />
+      <Route
+        exact
+        path="/learning_paths/:course_id/learning_objectives/:id"
+        component={LearningObjective}
+      />
+      <Route
+        exact
+        path="/learning_paths/:course_id/learning_objectives/:module_id/learning_events/:id"
+        component={LearningEvent}
+      />
+      <Route
+        exact
+        path="/learning_paths/:course_id/learning_objectives/:module_id/learning_events"
+        component={LearningEvents}
+      />
+      <Route
+          exact
+          path="/search"
+          component={SearchPage}
+      />
+      <Route component={Error} />
+    </Switch>
   );
 };
 
