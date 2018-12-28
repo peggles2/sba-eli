75b708227f4b5543b611d74a3561185a100dc7e3
Matthew Castle <matthew.ray.castle@gmail.com>
2018-12-28 12:13:17 -0800

    237 Added more targeted CSS and removed some important rules
    Targeted CSS better to remove styles with important in them

--- front-end/src/components/LearningEvent/LearningEventHeader.js
+++ front-end/src/components/LearningEvent/LearningEventHeader.js
@@ -21,19 +21,19 @@ class LearningEventHeader extends Component {
     const { event, topicTitle } = this.props;
     return (
       <Grid className="learning-event-header">
-        <Grid.Row>
+        <Grid.Row className="header-row">
           <Grid.Column>
             <div className="topic-title">{topicTitle}</div>
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row>
+        <Grid.Row className="header-row">
           <Grid.Column>
             <Header className="event-title" as="h1">
               {event.title}
             </Header>
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row>
+        <Grid.Row className="header-row">
           <Grid.Column width={12}>
             <div className="event-meta">Event Type (Time)</div>
           </Grid.Column>
@@ -42,7 +42,7 @@ class LearningEventHeader extends Component {
             {this.showDownloadButton(event)}
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row>
+        <Grid.Row className="header-row">
           <Grid.Column>
             <Divider />
           </Grid.Column>
