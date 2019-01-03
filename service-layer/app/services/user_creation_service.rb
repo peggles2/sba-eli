8f01b57ecaef09f0cad3e49963fe3954107a9c81
Andrew Wagner <awagner@Andrews-MBP.fios-router.home>
2019-01-03 13:06:23 -0500

    SBAELIDEV-326 improve some logging during user creation process

--- service-layer/app/services/user_creation_service.rb
+++ service-layer/app/services/user_creation_service.rb
@@ -17,11 +17,16 @@ class UserCreationService
   validates_confirmation_of :password
 
   def create
-    return false unless valid?
+    if !valid?
+      Rails.logger.debug("errors on user: #{errors.inspect}")
+      return false
+    end
+    Rails.logger.debug("creating user in cognito")
 
     begin
       CognitoService.sign_up(email, password)
     rescue Aws::CognitoIdentityProvider::Errors::UsernameExistsException
+      Rails.logger.debug("tried to create an existing cognito user")
       errors.add(:email, "already exists.")
       return false
     end
@@ -29,6 +34,7 @@ class UserCreationService
     # TODO: We're going to want to save the user_sub from the
     # cognito response as a custom attribute in canvas
 
+    Rails.logger.debug("trying to create user in canvas")
     # Create user in Canvas
     response = Canvas::User.create_user(build_canvas_json)
     self.user = User.from_canvas_json(response)
