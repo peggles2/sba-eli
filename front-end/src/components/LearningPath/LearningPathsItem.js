cfc64b2d751a2a9689059d9df8cc59b3fac9ad93
Alex Bradley <alex.robert.bradley@gmail.com>
2019-01-04 13:22:06 -0500

    Added placeholder Image to Learning Path cards

--- front-end/src/components/LearningPath/LearningPathsItem.js
+++ front-end/src/components/LearningPath/LearningPathsItem.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import { Card } from 'semantic-ui-react';
+import { Card, Image } from 'semantic-ui-react';
 
 const LearningPathsItem = (props) => {
   return(
     <Card>
+      <Image 
+        src={`/Image_Placeholder.png`} 
+        alt="learning path image placeholder"
+      />
       <Card.Content>
         <Card.Header><a href={`/learning_paths/${props.id}`}>{props.name}</a></Card.Header>
         <Card.Meta>{props.course_code}</Card.Meta>
+        <Card.Description>Maybe it means something more - something we can't yet understand.</Card.Description>
       </Card.Content>
     </Card>
   )
