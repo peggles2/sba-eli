d404c725005c22cf1ea744efece1a631cc5ce491
Alex Bradley <alex.robert.bradley@gmail.com>
2019-01-07 14:22:48 -0500

    Added Dashboard Header
    Added a Header to the Dashboard which says My Dashboard.

--- front-end/src/components/Dashboard/Dashboard.js
+++ front-end/src/components/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import LearningPaths from "../LearningPath/LearningPaths";
 import { Divider } from "semantic-ui-react";
+import DashboardHeader from "./DashboardHeader";
 import DashboardSplash from "./DashboardSplash";
 import DashboardCTA from "./DashboardCTA";
 import MetaTags from "../SEO/MetaTags";
@@ -14,6 +15,7 @@ export default class Dashboard extends Component {
         <MetaTags metaTitle="SBA Dashboard"
                   metaDescription="Description for the dashboard"
                   canonicalUrl="https://sba.gov/eli"/>
+        <DashboardHeader />
         <DashboardSplash />
         <DashboardCTA />
         <Divider hidden />
