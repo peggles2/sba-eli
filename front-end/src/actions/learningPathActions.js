ca329660f7b67124d3a5762310e65037a6bdf269
Matthew Castle <matthew.ray.castle@gmail.com>
2019-01-07 11:14:26 -0800

    349 Changed learning path component to utilize redux
    I modified to break out the fetch commands for the topics list and
    the learning path itself to be redux methods. Only recreating
    existing functionality to use redux

--- front-end/src/actions/learningPathActions.js
+++ front-end/src/actions/learningPathActions.js
@@ -1,10 +1,31 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseUrl = process.env.REACT_APP_SERVICE_HOST
+const baseUrl = process.env.REACT_APP_SERVICE_HOST;
 
 export function getLearningPaths() {
   return {
-    type: 'GET_LEARNING_PATHS',
+    type: "GET_LEARNING_PATHS",
     payload: axios.get(baseUrl + "/learning_paths")
+  };
+}
+
+export function getLearningPath(id) {
+  return {
+    type: "GET_LEARNING_PATH",
+    payload: axios.get(baseUrl + "/learning_paths/" + id)
+  };
+}
+
+export function getTopicsForPath(pathId) {
+  const topicParams = {
+    course_id: pathId
+  };
+
+  return {
+    type: "GET_TOPICS_FOR_LEARNING_PATH",
+    payload: axios.get(baseUrl + `/learning_objectives/`, {
+      params: topicParams
+    }),
+    topicPathId: pathId
   };
 }
--- front-end/src/reducers/learningPathReducer.js
+++ front-end/src/reducers/learningPathReducer.js
@@ -1,22 +1,87 @@
-import React from 'react';
+import React from "react";
 
 const defaultValue = {
   learningPaths: [],
   learningPathsLoading: false,
-  learningPathsError: null
+  learningPathsError: null,
+
+  learningPath: {},
+  learningPathLoading: false,
+  learningPathError: null,
+
+  topicsList: [],
+  pathTopicsLoading: true,
+  pathTopicsError: null
 };
 
-export default function reducer(state=defaultValue, action) {
-  switch(action.type) {
-    case 'GET_LEARNING_PATHS':
-      return {...state, learningPaths: [], learningPathsLoading: true, learningPathsError: null} 
-    case 'GET_LEARNING_PATHS_FAILURE':
-      return {...state, learningPaths: [], learningPathsLoading: false, learningPathsError: action.payload} 
-    case 'GET_LEARNING_PATHS_FULFILLED':
-      return {...state, learningPaths: action.payload.data, learningPathsLoading: false, learningPathsError: null} 
+export default function reducer(state = defaultValue, action) {
+  switch (action.type) {
+    case "GET_LEARNING_PATHS":
+      return {
+        ...state,
+        learningPaths: [],
+        learningPathsLoading: true,
+        learningPathsError: null
+      };
+    case "GET_LEARNING_PATHS_FAILURE":
+      return {
+        ...state,
+        learningPaths: [],
+        learningPathsLoading: false,
+        learningPathsError: action.payload
+      };
+    case "GET_LEARNING_PATHS_FULFILLED":
+      return {
+        ...state,
+        learningPaths: action.payload.data,
+        learningPathsLoading: false,
+        learningPathsError: null
+      };
+    case "GET_LEARNING_PATH":
+      return {
+        ...state,
+        learningPath: [],
+        learningPathLoading: true,
+        learningPathError: null
+      };
+    case "GET_LEARNING_PATH_FAILURE":
+      return {
+        ...state,
+        learningPath: [],
+        learningPathLoading: false,
+        learningPathError: action.payload
+      };
+    case "GET_LEARNING_PATH_FULFILLED":
+      return {
+        ...state,
+        learningPath: action.payload.data,
+        learningPathLoading: false,
+        learningPathError: null
+      };
+    case "GET_TOPICS_FOR_LEARNING_PATH":
+      return {
+        ...state,
+        topicsList: [],
+        pathTopicsLoading: true,
+        pathTopicsError: null
+      };
+    case "GET_TOPICS_FOR_LEARNING_PATH_FAILURE":
+      return {
+        ...state,
+        topicsList: [],
+        pathTopicsLoading: false,
+        pathTopicsError: action.payload
+      };
+    case "GET_TOPICS_FOR_LEARNING_PATH_FULFILLED":
+      return {
+        ...state,
+        topicsList: action.payload.data,
+        pathTopicsLoading: false,
+        pathTopicsError: null
+      };
     default:
       break;
-  };
-  
+  }
+
   return state;
 }
