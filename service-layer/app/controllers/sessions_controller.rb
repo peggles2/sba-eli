edbc854708f24406c784bb124facb2495433faf3
Nicholas Watson <nicholas.w.watson@me.com>
2019-01-10 15:32:11 -0500

    removed logging of cognito_reponse

--- service-layer/app/controllers/sessions_controller.rb
+++ service-layer/app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
 
   def create
     @cognito_response = CognitoService.authenticate(params[:email], params[:password])
-    Rails.logger.info @cognito_response
     canvas_user_response = Canvas::User.fetch_by_email(params[:email])
     sign_in User.from_canvas_json(canvas_user_response)
   rescue Aws::CognitoIdentityProvider::Errors::NotAuthorizedException,
