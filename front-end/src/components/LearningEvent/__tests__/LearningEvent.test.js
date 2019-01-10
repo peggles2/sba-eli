dcf6733d8265ed336f9f28e7614a2d4e15e12455
Matt Castle <matthew.ray.castle@gmail.com>
2019-01-10 11:44:01 -0500

    349 Fixed NavigationLearningPath and LearningEvent tests
    NavigationLearningPath test needed slight adjustment, and LearningEvent
    test needed to be reworked to account for redux work

--- front-end/src/components/LearningEvent/__tests__/LearningEvent.test.js
+++ front-end/src/components/LearningEvent/__tests__/LearningEvent.test.js
@@ -1,49 +1,60 @@
 import React from "react";
-import {Header} from "semantic-ui-react";
-import LearningEvent from "../LearningEvent";
+
+import { LearningEvent } from "../LearningEvent";
 import LearningEventManager from "../LearningEventManager";
 import MetaTags from "../../SEO/MetaTags";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
+import LearningEventHeader from "../LearningEventHeader";
+import LearningEventFooter from "../LearningEventFooter";
+
+function eventWrapper() {
+  const match = { params: { course_id: 1, module_id: 1, id: 1 } };
+  const title = "learning event title";
+  const desc = "learning event description";
+  const learningEvent = { title: title, description: desc };
+  const props = {
+    learningEvent,
+    match,
+
+    dispatch: () => {}
+  };
+
+  const wrapper = shallow(<LearningEvent {...props} />);
+
+  return { title, desc, wrapper };
+}
 
 describe("LearningEvent", () => {
-  
   it("should render meta tags with Learning Event specific information", () => {
-    const match = { params: { course_id: 1, module_id: 1, id: 1 } };
-    const title = "learning event title"
-    const desc = "learning event description"
-    const learningEvent= { title: title, description: desc};
-    
-    const wrapper = shallow(<LearningEvent match={match} />);
-    wrapper.instance().setState({learningEvent});
+    const { title, desc, wrapper } = eventWrapper();
 
     expect(wrapper.find(MetaTags).exists()).toBe(true);
     const metaWrapper = wrapper.find(MetaTags).dive();
 
-    expect(metaWrapper.find('title').text()).toBe(title);
+    expect(metaWrapper.find("title").text()).toBe(title);
     expect(metaWrapper.find("meta[name='author']").exists()).toBe(false);
     expect(metaWrapper.find("meta[name='description']").exists()).toBe(true);
-    expect(metaWrapper.find("meta[name='description']").props().content).toBe(desc);
+    expect(metaWrapper.find("meta[name='description']").props().content).toBe(
+      desc
+    );
     //TODO: add Canonical URL validation when we have final urls
     //expect(metaWrapper.find("link[rel='canonical']").props().href).toBe("https://sba.gov/eli")
   });
 
-  it("should render a single h1 and it should display the event title", () => {
-    const match = { params: { course_id: 1, module_id: 1, id: 1 } };
-    const title = "learning event title"
-    const desc = "learning event description"
-    const learningEvent = { title: title, description: desc};
-    
-    const wrapper = mount(<LearningEvent match={match} />);
-    wrapper.instance().setState({learningEvent});
+  it("should render a LearningEventHeader", () => {
+    const { wrapper } = eventWrapper();
 
-    expect(wrapper.find("h1").exists()).toBe(true);
-    expect(wrapper.find("h1").length).toEqual(1);
-    expect(wrapper.find("h1").text()).toEqual(title);
+    expect(wrapper.find(LearningEventHeader).exists()).toBe(true);
+  });
+
+  it("should render a LearningEventFooter", () => {
+    const { wrapper } = eventWrapper();
+
+    expect(wrapper.find(LearningEventFooter).exists()).toBe(true);
   });
 
   it("should render a <LearningEventManager>", () => {
-    const match = { params: { course_id: 1, module_id: 1, id: 1 } };
-    const wrapper = mount(<LearningEvent match={match} />);
+    const { wrapper } = eventWrapper();
 
     expect(wrapper.find(LearningEventManager).exists()).toBe(true);
   });
--- front-end/src/components/Navigation/__tests__/NavigationLearningPath.test.js
+++ front-end/src/components/Navigation/__tests__/NavigationLearningPath.test.js
@@ -1,30 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Dropdown } from 'semantic-ui-react';
-import NavigationLearningPath from '../NavigationLearningPath';
-import { shallow, mount } from 'enzyme';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Dropdown } from "semantic-ui-react";
+import NavigationLearningPath from "../NavigationLearningPath";
+import { shallow, mount } from "enzyme";
 
-describe('NavigationLearningPath', () => {
-  it('should render correctly', () => {
-    const wrapper = shallow(<NavigationLearningPath/>);
-    
+describe("NavigationLearningPath", () => {
+  it("should render correctly", () => {
+    const wrapper = shallow(<NavigationLearningPath />);
+
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should render a <Dropdown>', () => {
+  it("should render a <Dropdown>", () => {
     const wrapper = mount(
-                      <Router>
-                        <NavigationLearningPath/>
-                      </Router>);
+      <Router>
+        <NavigationLearningPath />
+      </Router>
+    );
 
     const learningpaths = [
-      {id: 1, name: "Learning Path 1"},
-      {id: 2, name: "Learning Path 2"},
-      {id: 3, name: "Learning Path 3"}
-    ]
+      { id: 1, name: "Learning Path 1" },
+      { id: 2, name: "Learning Path 2" },
+      { id: 3, name: "Learning Path 3" }
+    ];
 
-    wrapper.find(NavigationLearningPath).setState({learningPaths: learningpaths})
+    wrapper
+      .find(NavigationLearningPath)
+      .setState({ learningPaths: learningpaths });
 
-    expect(wrapper.find(Dropdown.Item).length).toEqual(3);
+    expect(wrapper.find(Dropdown.Item).length).toEqual(6);
   });
 });
