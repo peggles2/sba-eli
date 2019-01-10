9e5aa359ec1061b8302fe7a3da16ce8093c5d1ca
Alex Bradley <alex.robert.bradley@gmail.com>
2019-01-10 10:08:33 -0500

    Changed LearningPathsItem into a class
    Changed LPI into a react class.

--- front-end/src/components/LearningPath/LearningPathsItem.js
+++ front-end/src/components/LearningPath/LearningPathsItem.js
@@ -1,24 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Card, Image } from "semantic-ui-react";
 import LearningPathProgress from "./LearningPathProgress";
 
-const LearningPathsItem = (props) => {
-  return(
-    <Card>
-      <Image
-        src={`/Image_Placeholder.png`}
-        alt="learning path image placeholder"
-      />
-      <Card.Content>
-        <Card.Header><a href={`/learning_paths/${props.id}`}>{props.name}</a></Card.Header>
-        <Card.Meta>{props.course_code}</Card.Meta>
-        <Card.Description>Maybe it means something more - something we can't yet understand.</Card.Description>
-      </Card.Content>
-      <Card.Content>
-        <LearningPathProgress complete={4} total={5}/>
-      </Card.Content>
-    </Card>
-  )
-}
+class LearningPathsItem extends Component {
+  state = { isLoggedIn: true }
+
+  learningPathProgress = () => {
+    if(this.state.isLoggedIn) {
+      return (
+        <Card.Content>
+          <LearningPathProgress complete={4} total={5}/>
+        </Card.Content>
+      );
+    }
+  };
+
+  render() {
+    const complete = 3;
+    const total = 5;
+    const learningPathDescription = "Maybe it means something more - something we can't yet understand."
+
+    return(
+      <Card>
+        <Image
+          src={`/Image_Placeholder.png`}
+          alt="learning path image placeholder"
+        />
+        <Card.Content>
+          <Card.Header aria-label={`Link to Learning Path ` + this.props.name}><a href={`/learning_paths/${this.props.id}`}>{this.props.name}</a></Card.Header>
+          <Card.Meta>{this.props.course_code}</Card.Meta>
+          <Card.Description>{learningPathDescription}</Card.Description>
+        </Card.Content>
+        {this.learningPathProgress()}
+      </Card>
+    )
+  }
+};
 
 export default LearningPathsItem;
