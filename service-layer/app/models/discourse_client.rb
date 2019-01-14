1c91006e6d9ab40b892f29bad7850e602cc61e99
Nicholas Watson <nicholas.w.watson@me.com>
2019-01-14 13:39:35 -0500

    user and key are assigned wrong

--- service-layer/app/models/discourse_client.rb
+++ service-layer/app/models/discourse_client.rb
@@ -1,8 +1,8 @@
 module DiscourseClient
   def self.create
     client = DiscourseApi::Client.new(ENV["DISCOURSE_HOST"])
-    client.api_key = ENV["DISCOURSE_USER"]
-    client.api_username = ENV["DISCOURSE_API_KEY"]
+    client.api_username = ENV["DISCOURSE_USER"]
+    client.api_key = ENV["DISCOURSE_API_KEY"]
     client
   end
 end
