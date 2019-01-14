8b467a18af661f2226acd89ba580f16b6f6c0a8a
Matthew Castle <matthew.ray.castle@gmail.com>
2019-01-14 09:12:18 -0800

    349 Fix enrolls_controller to adhere to rubocop requirements

--- service-layer/app/controllers/enrolls_controller.rb
+++ service-layer/app/controllers/enrolls_controller.rb
@@ -5,7 +5,9 @@ class EnrollsController < ApplicationController
     enroll_response = Canvas::LearningPath.enroll(params[:learning_path_id], Current.user)
     path_response = Canvas::LearningPath.find(params[:learning_path_id])
     topics_response = Canvas::LearningObjective.all(params[:learning_path_id])
-    
-    render json: { "enrollment": enroll_response, "learningPath": path_response, "topicsList": topics_response }
+
+    render json: { "enrollment": enroll_response,
+                   "learningPath": path_response,
+                   "topicsList": topics_response }
   end
 end
