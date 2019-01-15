619f2f834d306ffd9fcd3d454bde2ebc9e686570
Alex Bradley <alex.robert.bradley@gmail.com>
2019-01-15 11:58:56 -0500

    356 Get total number of MLEs
    Created functions in LearningPathsItem to get the total number of Micro
    Learning Events for a Learning Path.

--- front-end/src/components/LearningPath/LearningPathsItem.js
+++ front-end/src/components/LearningPath/LearningPathsItem.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 import { Card, Image } from "semantic-ui-react";
 import LearningPathProgress from "./LearningPathProgress";
+import axios from "axios";
 import { withRouter } from 'react-router-dom';
 import { connect } from "react-redux";
 
+import { getLearningEvents } from "../../actions/learningEventActions";
+
 export class LearningPathsItem extends Component {
+  state = { topicsList: [] }
+
+  componentDidMount() {
+    this.getTopicsList();
+  }
 
   learningPathProgress = (total) => {
-    if(this.props.isUserLoggedIn) {
+    // const isUserLoggedIn = this.props.isUserLoggedIn
+    const isUserLoggedIn = true;
+    if(isUserLoggedIn) {
       const complete = 0;
 
       return (
@@ -18,8 +28,31 @@ export class LearningPathsItem extends Component {
     }
   };
 
+  getTopicsList = () => {
+    const baseUrl = process.env.REACT_APP_SERVICE_HOST;
+
+    axios.get(baseUrl + `/learning_objectives/`, {params: {course_id: this.props.id}})
+      .then(res => {
+        const topicsList = res.data
+        this.setState({ topicsList })
+      })
+  }
+
+  totalMicroLearningEvents = () => {
+    const reducer = (totalList, number) => totalList + number;
+    const topicsList = this.state.topicsList;
+
+    if(topicsList.length > 0) {
+      const total = topicsList.map(topic => topic.items_count).reduce(reducer);
+
+      return total;
+    } else {
+      return 0
+    }
+  }
+
   render() {
-    const total = 5;
+    const total = this.totalMicroLearningEvents();
     const learningPathDescription = "Maybe it means something more - something we can't yet understand."
 
     return(
@@ -42,7 +75,9 @@ export class LearningPathsItem extends Component {
 };
 
 const mapStateToProps = (store) => {
-  return { isUserLoggedIn: store.login.isUserLoggedIn }
+  return {
+    isUserLoggedIn: store.login.isUserLoggedIn,
+  }
 }
 
 export default withRouter(connect(mapStateToProps)(LearningPathsItem));
