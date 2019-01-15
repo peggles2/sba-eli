f8a575f38615c1e86673bd622d0a9d96c5c937b9
Nicholas Watson <nicholas.w.watson@me.com>
2019-01-15 13:36:01 -0500

    changed discussion_id from integer to string

--- service-layer/db/migrate/20190110161517_create_fearless_discussion_map.rb
+++ service-layer/db/migrate/20190110161517_create_fearless_discussion_map.rb
@@ -1,7 +1,7 @@
 class CreateFearlessDiscussionMap < ActiveRecord::Migration[5.2]
   def change
     create_table :fearless_discussion_maps do |t|
-      t.integer :discussion_id, null: false
+      t.string :discussion_id, null: false
       t.string :content_type, null: false
       t.integer :content_id, null: false
       t.timestamps
