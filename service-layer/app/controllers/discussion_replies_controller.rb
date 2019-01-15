d6644054503a5049764c322b0cd7cd529cfb7229
Nicholas Watson <nicholas.w.watson@me.com>
2019-01-15 09:16:33 -0500

    added api methods to create discussion replies

---
+++ service-layer/app/controllers/discussion_replies_controller.rb
@@ -0,0 +1,24 @@
+class DiscussionRepliesController < ApplicationController
+  before_action :valid_session?
+
+  def show
+    response = DiscourseClient.create.get_post(params[:id])
+    render json: response
+  end
+
+  def create
+    reply = DiscussionReply.new(model_params.merge!(user: Current.user))
+    reply.discussion_id = params[:discussion_id]
+    if reply.create
+      render json: reply.response, status: :created
+    else
+      render errors_for(reply)
+    end
+  end
+
+  private
+
+  def model_params
+    params.require(:discussion_reply).permit(:discussion_id, :raw)
+  end
+end
