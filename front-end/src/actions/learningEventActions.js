60358bc7bf62ac4beab6c946921ac49e846db70c
Matt Castle <matthew.ray.castle@gmail.com>
2019-01-15 16:12:52 -0500

    349 Added authorization headers to all path and event actions
    With expanded support for session more calls will need authorization
    headers added for them to work properly, adjusted learningPathActions
    and learningEventActions to now add authorization in axios config
    when logged in

--- front-end/src/actions/learningEventActions.js
+++ front-end/src/actions/learningEventActions.js
@@ -1,40 +1,56 @@
 import axios from "axios";
 
-const baseUrl = process.env.REACT_APP_SERVICE_HOST;
+function axiosConfig(state, params = null) {
+  let authConfig = {};
+  if (state.login.isUserLoggedIn) {
+    const { access_token } = state.login.userData;
+    authConfig = { headers: { AUTHORIZATION: access_token } };
+  }
+
+  return {
+    params: params,
+    baseURL: process.env.REACT_APP_SERVICE_HOST,
+    ...authConfig
+  };
+}
 
 export function getLearningEvent(course_id, module_id, event_id) {
-  const url = baseUrl + `/learning_events/${event_id}`;
+  const url = `/learning_events/${event_id}`;
 
   const eventParams = {
     course_id,
     module_id
   };
-  return {
-    type: "GET_LEARNING_EVENT",
-    payload: axios.get(url, { params: eventParams })
+  return (dispatch, getState) => {
+    dispatch({
+      type: "GET_LEARNING_EVENT",
+      payload: axios.get(url, axiosConfig(getState(), eventParams))
+    });
   };
 }
 
 export function getLearningEvents(course_id, module_id) {
-  const url = baseUrl + `/learning_events/`;
+  const url = `/learning_events/`;
   const eventParams = {
     course_id,
     module_id
   };
 
-  return {
-    type: "GET_LEARNING_EVENTS",
-    payload: axios.get(url, { params: eventParams })
+  return (dispatch, getState) => {
+    dispatch({
+      type: "GET_LEARNING_EVENTS",
+      payload: axios.get(url, axiosConfig(getState(), eventParams))
+    });
   };
 }
 
 export function completeLearningEvent(path_id, objective_id, event_id) {
-  const url =
-    baseUrl +
-    `/learning_paths/${path_id}/learning_objectives/${objective_id}/learning_events/${event_id}/done`;
+  const url = `/learning_paths/${path_id}/learning_objectives/${objective_id}/learning_events/${event_id}/done`;
 
-  return {
-    type: "COMPLETE_LEARNING_EVENT",
-    payload: axios.post(url)
+  return (dispatch, getState) => {
+    dispatch({
+      type: "COMPLETE_LEARNING_EVENT",
+      payload: axios.post(url, {}, axiosConfig(getState()))
+    });
   };
 }
