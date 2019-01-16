b86d869ba6ced8f4b88c6a2e14958d16c1868dac
Andrew Wagner <awagner@Andrews-MBP.fios-router.home>
2019-01-16 16:50:33 -0500

    handle cases where password is null

--- service-layer/app/validators/password_complexity_validator.rb
+++ service-layer/app/validators/password_complexity_validator.rb
@@ -1,5 +1,7 @@
 class PasswordComplexityValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
+    value = '' if value.nil?
+    
     unless /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+/.match?(value)
       default = "Must include at least one lowercase letter, one uppercase letter, and one digit"
       record.errors[attribute] << (options[:message] || default)
