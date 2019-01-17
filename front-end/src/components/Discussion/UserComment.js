dd8ac3091af6d80f066fbecd1e6edbc0dde610d7
Kelly <kdyer@users.noreply.github.com>
2019-01-16 23:22:56 -0500

    Format the date on the discussion posts

--- front-end/src/components/Discussion/UserComment.js
+++ front-end/src/components/Discussion/UserComment.js
@@ -24,6 +24,27 @@ export class UserComment extends Component {
     return "http://picsum.photos/45"
   }
 
+  formatDate(timestamp) {
+    var month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+    
+    if (timestamp) {
+      const date = new Date(timestamp)
+      return month[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes()
+    }
+  }
+
   render() {
     var reply = this.props.replies
 
@@ -42,7 +63,7 @@ export class UserComment extends Component {
             <Grid.Column className='username'>
               {reply.user_name}
               <span className='post-date'>
-                {reply.timestamp}
+                {this.formatDate(reply.timestamp)}
                 </span>
             </Grid.Column>
           </Grid.Row>
