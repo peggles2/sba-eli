ce33281db28e2a83df07010bf17027d426a5d195
Kelly <kdyer@users.noreply.github.com>
2019-01-22 15:41:21 -0500

    Add a check to only insert comments if there is data to display

--- front-end/src/components/Discussion/UserComment.js
+++ front-end/src/components/Discussion/UserComment.js
@@ -48,35 +48,38 @@ export class UserComment extends Component {
   render() {
     var reply = this.props.replies
 
-    return <Grid.Row className='user-comment' width={16}>
-      <Grid.Column width={1}>
-        <Image className='user-image'
-               size='tiny'
-               circular
-               verticalAlign='middle'
-               src={this.userImage(reply.user_img)}
-               alt={reply.user_name}/>
-      </Grid.Column>
-      <Grid.Column width={14} className='discussion-post'>
-        <Grid>
-          <Grid.Row columns={2} className='user'>
-            <Grid.Column className='username'>
-              {reply.user_name}
-              <span className='post-date'>
-                {this.formatDate(reply.timestamp)}
-                </span>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row className='user-title'>
-            {reply.user_title}
-          </Grid.Row>
-          <Grid.Row>
-            {reply.body}
-          </Grid.Row>
-          {this.replyLink(this.props.parent_content_type, this.props.parent_id, reply)}
-        </Grid>
-      </Grid.Column>
-    </Grid.Row>
+    if (reply && reply.length && reply.length > 0) { 
+      return <Grid.Row className='user-comment' width={16}>
+        <Grid.Column width={1}>
+          <Image className='user-image'
+                size='tiny'
+                circular
+                verticalAlign='middle'
+                src={this.userImage(reply.user_img)}
+                alt={reply.user_name}/>
+        </Grid.Column>
+        <Grid.Column width={14} className='discussion-post'>
+          <Grid>
+            <Grid.Row columns={2} className='user'>
+              <Grid.Column className='username'>
+                {reply.user_name}
+                <span className='post-date'>
+                  {this.formatDate(reply.timestamp)}
+                  </span>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row className='user-title'>
+              {reply.user_title}
+            </Grid.Row>
+            <Grid.Row>
+              {reply.body}
+            </Grid.Row>
+            {this.replyLink(this.props.parent_content_type, this.props.parent_id, reply)}
+          </Grid>
+        </Grid.Column>
+      </Grid.Row>
+    } 
+    return null;
   }
 }
 
