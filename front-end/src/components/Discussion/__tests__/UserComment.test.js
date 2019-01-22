d14a13145c144b9f3984c0a88699b1f34e346fe2
Kelly <kdyer@users.noreply.github.com>
2019-01-22 15:29:12 -0500

    Update the User Comment tests now that we’re using redux

--- front-end/src/components/Discussion/__tests__/UserComment.test.js
+++ front-end/src/components/Discussion/__tests__/UserComment.test.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
-import UserComment from "../UserComment";
+import {UserComment} from "../UserComment";
 
-describe('UserComment', () => {
+describe('Discussion: User Comment View', () => {
+  it('should not render any replies', () => {
+    const mleDiscussion = {
+      post_count: 0,
+      replies: []
+    }
+    const wrapper = mount( < UserComment 
+      parent_content_type = "learning_event"
+      parent_id = "8"
+      replies = {mleDiscussion.replies}/>);
+    expect(wrapper.find('.user-comment').length).toBe(0);
+  });
+
   it('should render the correct amount of top level replies', () => {
     const mleDiscussion = {
-      post_count: 5,
+      post_count: 3,
       replies: [
         {
           id: 123,
           content_type: "comment",
-          user_img: "http://picsum.photos/45?random",
+          user_img: "http://picsum.photos/60?random",
           user_name: "Georgina Foreman",
-          timestamp: "September 1, 2018 1pm",
+          timestamp: "2019-01-16T22:22:13.619Z",
           user_title: "Small Business Owner Extraordinaire",
           post_number: 1,
           reply_to_post_number: null,
@@ -21,9 +33,9 @@ describe('UserComment', () => {
             {
               id: 5678,
               content_type: "comment",
-              user_img: "http://picsum.photos/45?random",
+              user_img: "http://picsum.photos/60?random",
               user_name: "Penelope Grant",
-              timestamp: "January 1, 2018 1pm",
+              timestamp: "2019-01-16T22:22:13.619Z",
               user_title: "Small Business Person",
               post_number: 3,
               reply_to_post_number: 1,
@@ -35,9 +47,9 @@ describe('UserComment', () => {
         {
           id: 234,
           content_type: "comment",
-          user_img: "http://picsum.photos/45?random",
+          user_img: "http://picsum.photos/60?random",
           user_name: "Regina Miles",
-          timestamp: "September 1, 2018 1:05pm",
+          timestamp: "2019-01-16T22:22:13.619Z",
           user_title: "Medium Business Owner",
           post_number: 2,
           reply_to_post_number: null,
@@ -47,38 +59,71 @@ describe('UserComment', () => {
       ]
     }
     const wrapper = mount(<UserComment
-        parent_content_type={mleDiscussion.content_type}
-        parent_id={mleDiscussion.id}
+        parent_content_type="learning_event"
+        parent_id="8"
         replies={mleDiscussion.replies}/>);
     expect(wrapper.find('.user-comment').length).toBe(2);
   });
+});
 
+describe("Discussion when user is logged in", () => {
   it('should render all the parts of a user comment including the reply link', () => {
     const mleDiscussion = {
-      id: 987,
-      content_type: "learning event",
-      reply_count: 234,
+      post_count: 1,
       replies: [
         {
           id: 234,
           content_type: "comment",
-          user_img: "http://picsum.photos/45?random",
-          user_name: "Regina Miles",
-          timestamp: "September 1, 2018 1:05pm",
+          user_img: "http://picsum.photos/60?random",
+          user_name: "Angel Alvarez",
+          timestamp: "2019-01-16T22:22:13.619Z",
           user_title: "Medium Business Owner",
+          post_number: 1,
+          reply_to_post_number: null,
           body: "Empty replies array. Gummi bears sugar plum biscuit chocolate cake dessert apple pie. Liquorice wafer gummi bears dessert muffin tootsie roll powder brownie. Lemon drops macaroon fruitcake",
           replies: []
         }
       ]
     }
     const wrapper = shallow(<UserComment
-        parent_content_type={mleDiscussion.content_type}
-        parent_id={mleDiscussion.id}
+        isUserLoggedIn={true}
+        parent_content_type = "learning_event"
+        parent_id = "8"
         replies={mleDiscussion.replies}/>);
     expect(wrapper.find('.user-image').exists()).toBe(true);
     expect(wrapper.find('.username').exists()).toBe(true);
     expect(wrapper.find('.post-date').exists()).toBe(true);
     expect(wrapper.find('.user-title').exists()).toBe(true);
-    //expect(wrapper.find('.reply-link').exists()).toBe(true);
+    expect(wrapper.find('.reply-link').exists()).toBe(true);
   });
+});  
+  
+describe("Discussion when user is NOT logged in", () => {
+  it('should render all the parts of a user comment without the reply link', () => {
+    const mleDiscussion = {
+      post_count: 1,
+      replies: [{
+        id: 234,
+        content_type: "comment",
+        user_img: "http://picsum.photos/45?random",
+        user_name: "Misha Wells",
+        timestamp: "September 1, 2018 1:05pm",
+        user_title: "Medium Business Owner",
+        post_number: 1,
+        reply_to_post_number: null,
+        body: "Empty replies array. Gummi bears sugar plum biscuit chocolate cake dessert apple pie. Liquorice wafer gummi bears dessert muffin tootsie roll powder brownie. Lemon drops macaroon fruitcake",
+        replies: []
+      }]
+    }
+    const wrapper = shallow(<UserComment
+        isUserLoggedIn={false}
+        parent_content_type = "learning_event"
+        parent_id = "8"
+        replies={mleDiscussion.replies}/>);
+    expect(wrapper.find('.user-image').exists()).toBe(true);
+    expect(wrapper.find('.username').exists()).toBe(true);
+    expect(wrapper.find('.post-date').exists()).toBe(true);
+    expect(wrapper.find('.user-title').exists()).toBe(true);
+    expect(wrapper.find('.reply-link').exists()).toBe(false);
+    });
 });
