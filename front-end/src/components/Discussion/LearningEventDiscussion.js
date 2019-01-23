6b4a27b43f3819c8fb61ba4cde116387e8f0ed10
Kelly <kdyer@users.noreply.github.com>
2019-01-23 13:05:08 -0500

    Fix this.props call variable declaration

--- front-end/src/components/Discussion/LearningEventDiscussion.js
+++ front-end/src/components/Discussion/LearningEventDiscussion.js
@@ -10,31 +10,31 @@ import "./LearningEventDiscussion.scss";
 export class LearningEventDiscussion extends Component {
 
   commentCount() {
-    const properties = this.props
-    if (properties.replies && properties.reply_count > 0) {
-      return <span className="comment-count">({properties.reply_count})</span>
+    const {replies, reply_count} = this.props
+    if (replies && reply_count > 0) {
+      return <span className="comment-count">({reply_count})</span>
     }
     return null
   }
 
   componentDidUpdate(prevProps) {
-    const properties = this.props
-    if ((properties.parent_id !== prevProps.parent_id) &&
-        (properties.parent_content_type !== prevProps.parent_content_type)) {
-      properties.dispatch(getDiscussion(properties.parent_content_type, properties.parent_id))
+    const {parent_content_type, parent_id} = this.props
+    if ((parent_id !== prevProps.parent_id) &&
+        (parent_content_type !== prevProps.parent_content_type)) {
+      this.props.dispatch(getDiscussion(parent_content_type, parent_id))
     }
   }
 
   componentDidMount() {
-    const properties = this.props
-    if ((properties.parent_id !== null) &&
-        (properties.parent_content_type !== null)) {
-      properties.dispatch(getDiscussion(properties.parent_content_type, properties.parent_id))
+    const {parent_content_type, parent_id} = this.props
+    if ((parent_id !== null) &&
+        (parent_content_type !== null)) {
+      this.props.dispatch(getDiscussion(parent_content_type, parent_id))
     }
   }
 
   render() {
-    const properties = this.props
+    const {replies, parent_content_type, parent_id} = this.props
     return (
         <Grid id="mle-comments">
           <Grid.Row stretched centered columns={15} className="mle-comments-header-row">
@@ -44,9 +44,9 @@ export class LearningEventDiscussion extends Component {
             </Grid.Column>
           </Grid.Row>
           <DiscussionPost/>
-          <Discussion replies={properties.replies}
-                      parent_content_type={properties.parent_content_type}
-                      parent_id={properties.parent_id}/>
+          <Discussion replies={replies}
+                      parent_content_type={parent_content_type}
+                      parent_id={parent_id}/>
         </Grid>
     )
   }
