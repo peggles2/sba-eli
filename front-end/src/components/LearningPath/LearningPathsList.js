50ebce7d07f50e04692c723228a109cd3160422f
Matt Castle <matthew.ray.castle@gmail.com>
2019-01-29 12:16:49 -0500

    Formatting update before making changes

--- front-end/src/components/LearningPath/LearningPathsList.js
+++ front-end/src/components/LearningPath/LearningPathsList.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Card } from 'semantic-ui-react';
+import React from "react";
+import { Card } from "semantic-ui-react";
 import { connect } from "react-redux";
-import LearningPathsItem from './LearningPathsItem';
+import LearningPathsItem from "./LearningPathsItem";
 
-import { getLearningPaths } from '../../actions/learningPathActions';
+import { getLearningPaths } from "../../actions/learningPathActions";
 
 export class LearningPathsList extends React.Component {
   componentDidMount() {
@@ -11,25 +11,26 @@ export class LearningPathsList extends React.Component {
   }
 
   render() {
-    let result = <h3>There are no learning paths available to view at this time</h3>
+    let result = (
+      <h3>There are no learning paths available to view at this time</h3>
+    );
     if (this.props.learningPaths.length > 0) {
-      result = this.props.learningPaths.map(c => <LearningPathsItem key={c.id}
-                                                                    id={c.id} 
-                                                                    name={c.name} 
-                                                                    course_code={c.course_code} />
-        )
-      }
+      result = this.props.learningPaths.map(c => (
+        <LearningPathsItem
+          key={c.id}
+          id={c.id}
+          name={c.name}
+          course_code={c.course_code}
+        />
+      ));
+    }
 
-    return(
-      <Card.Group itemsPerRow={3}>
-        {result}
-      </Card.Group>
-    )
+    return <Card.Group itemsPerRow={3}>{result}</Card.Group>;
   }
 }
 
-export default connect((store) => {
+export default connect(store => {
   return {
     learningPaths: store.learningPath.learningPaths
-  }
+  };
 })(LearningPathsList);
