751879385cb3244f1f57b9e6db5ac9cc027d30b4
Matthew Castle <matthew.ray.castle@gmail.com>
2019-01-29 15:44:14 -0800

    Added validators to assessment.rb file

--- service-layer/app/models/assessment.rb
+++ service-layer/app/models/assessment.rb
@@ -14,4 +14,13 @@
 #
 
 class Assessment < FearlessRecord
+  validates :course_id, :quiz_id, :name, presence: true
+  validates :minimum, :maximum, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validate :minimum_is_less_than_maximum
+
+  def minimum_is_less_than_maximum
+    if minimum.present? && maximum.present? && minimum >= maximum
+      errors.add(:minimum, "minimum must be less than maximum")
+    end
+  end
 end
