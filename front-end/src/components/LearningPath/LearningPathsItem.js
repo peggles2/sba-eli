905cfec4428906602bdd202a33a148930492c2ba
Matt Castle <matthew.ray.castle@gmail.com>
2019-01-29 12:07:17 -0500

    Formatting changes before beginning work

--- front-end/src/components/LearningPath/LearningPathsItem.js
+++ front-end/src/components/LearningPath/LearningPathsItem.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react'
-import { Card, Image } from 'semantic-ui-react';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { Card, Image } from "semantic-ui-react";
+import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 
 export class LearningPathsItem extends Component {
   render() {
     return (
-      <Card href={'/learning_paths/' + this.props.id}>
-        <Image 
-          src={`/Image_Placeholder.png`} 
+      <Card href={"/learning_paths/" + this.props.id}>
+        <Image
+          src={`/Image_Placeholder.png`}
           alt="learning path image placeholder"
         />
         <Card.Content>
           <Card.Header>{this.props.name}</Card.Header>
           <Card.Meta>{this.props.course_code}</Card.Meta>
-          <Card.Description>Maybe it means something more - something we can't yet understand.</Card.Description>
+          <Card.Description>
+            Maybe it means something more - something we can't yet understand.
+          </Card.Description>
         </Card.Content>
       </Card>
-    )
+    );
   }
 }
 
-export default connect((store) => {
-  return {}
+export default connect(store => {
+  return {};
 })(withRouter(LearningPathsItem));
