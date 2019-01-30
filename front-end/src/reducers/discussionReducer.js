9fd5473583a3c7f7de6c99f7909eb4cc6206c3d4
Kelly <kdyer@users.noreply.github.com>
2019-01-30 15:24:28 -0500

    Format the values in DiscussionReducer

--- front-end/src/reducers/discussionReducer.js
+++ front-end/src/reducers/discussionReducer.js
@@ -6,11 +6,23 @@ const defaultValue = {
 export default function reducer(state = defaultValue, action) {
   switch (action.type) {
     case 'GET_DISCUSSION':
-      return {...state, replies: [], reply_count: 0}
+      return {
+        ...state, 
+        replies: [], 
+        reply_count: 0
+      }
     case 'GET_DISCUSSION_FAILURE':
-      return {...state, replies: [], reply_count: 0}
+      return {
+        ...state, 
+        replies: [], 
+        reply_count: 0
+      }
     case 'GET_DISCUSSION_FULFILLED':
-      return {...state, replies: action.payload.data.replies, reply_count: action.payload.data.post_count}
+      return {
+        ...state, 
+        replies: action.payload.data.replies, 
+        reply_count: action.payload.data.post_count
+      }
     default:
       break;
   };
