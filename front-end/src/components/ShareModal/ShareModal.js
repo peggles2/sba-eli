f0cb094de6cb0a874db28ec769686ba73374b5d2
Nicholas Watson <nicholas.w.watson@me.com>
2019-01-30 17:33:20 -0500

    style changes

--- front-end/src/components/ShareModal/ShareModal.js
+++ front-end/src/components/ShareModal/ShareModal.js
@@ -14,103 +14,112 @@ export default class ShareModal extends React.Component {
   constructor(props) {
     super(props);
     this.locationRef = React.createRef();
-  }
-
-  state = {
-    modalOpen: false,
-    textHasBeenCopied: ''
+    this.state = {
+      modalOpen: false,
+      copyButtonText: 'Copy'
+    }
   }
 
   handleOpen = () => this.setState({ modalOpen: true })
 
   handleClose = () => {
     this.setState({ modalOpen: false })
-    this.setState({ textHasBeenCopied: '' })
+    this.setState({ copyButtonText: 'Copy' })
   }
 
   handleCopy = (e) =>  {
     var locationValue = this.locationRef.current;
     locationValue.select();
     document.execCommand("copy");
-    this.setState({ textHasBeenCopied: 'Link has been copied to clipboard!' })
+    this.setState({ copyButtonText: 'Copied!' })
   }
 
   render() {
     return <Modal trigger={<Button onClick={this.handleOpen} className="share-modal-button mobile hidden">Share &nbsp; <Icon name={"share"} /></Button>}
-                  id="ShareModal"
+                  id="shareModal"
                   open={this.state.modalOpen}
                   onClose={this.handleClose}
                   closeOnDimmerClick={false}
                   size={'tiny'} >
-      <Modal.Header>
+      <Modal.Content>
         <Grid>
-          <Grid.Row>
-            <Grid.Column width={15}>
-              Share this {this.props.contentType}
+          <Grid.Row stretched columns={16}>
+            <Grid.Column width={14}>
+              <h2>Share this {this.props.contentType}</h2>
             </Grid.Column>
-            <Grid.Column width={1}>
-              <Button content='X' size={'small'} onClick={this.handleClose} className="share-close-button" />
+            <Grid.Column width={2}>
+              <Button className='share-close-button' onClick={this.handleClose} icon><Icon link name='close' size='large'/></Button>
             </Grid.Column>
           </Grid.Row>
         </Grid>
-      </Modal.Header>
-
+        <hr/>
         <p>
           {this.props.description}
         </p>
-        <div className="share-model-button-group">
-          <FacebookShareButton
-            url={this.props.location}
-            quote={this.props.description}
-            className="share-modal-share">
-            <Icon
-              name="facebook official"
-              size="huge"
-              className="share-facebook-icon" />
-              <p>Facebook</p>
-          </FacebookShareButton>
-          <TwitterShareButton
-            url={this.props.location}
-            title={this.props.description}
-            className="share-modal-share">
-            <Icon
-              name="twitter"
-              size="huge"
-              className="share-twitter-icon" />
-              <p>Twitter</p>
-          </TwitterShareButton>
-          <LinkedinShareButton
-            url={this.props.location}
-            title={this.props.description}
-            windowWidth={750}
-            windowHeight={600}
-            className="share-modal-share">
-            <Icon
-              name="linkedin"
-              size="huge"
-              className="share-linkedin-icon" />
-              <p>LinkedIn</p>
-          </LinkedinShareButton>
-          <EmailShareButton
-            subject={this.props.title}
-            body={this.props.description}
-            url={this.props.location}
-            className="share-modal-share">
-            <Icon
-              name="mail"
-              size="huge" />
-              <p>Email</p>
-          </EmailShareButton>
-        </div>
-        <Grid columns="equal">
-            <Grid.Column></Grid.Column>
-            <Grid.Column width={14}>
-              <Input size='small' ref={this.locationRef} defaultValue={this.props.location} className="share-text-input" />
-              {this.state.textHasBeenCopied}
-              <Button content='Copy' size={'small'} onClick={this.handleCopy} className="share-copy-button" />
+        <Grid>
+          <Grid.Row columns={16} className="share-method-group">
+            <Grid.Column width={4} className="first-share-column share-method-column">
+              <FacebookShareButton
+                url={this.props.location}
+                quote={this.props.description}
+                className="share-modal-share">
+                <Icon
+                  name="facebook official"
+                  size="huge"
+                  className="share-facebook-icon" />
+                  <p>Facebook</p>
+              </FacebookShareButton>
             </Grid.Column>
-            <Grid.Column></Grid.Column>
+            <Grid.Column width={4}>
+              <TwitterShareButton
+                url={this.props.location}
+                title={this.props.description}
+                className="share-modal-share">
+                <Icon
+                  name="twitter"
+                  size="huge"
+                  className="share-twitter-icon" />
+                  <p>Twitter</p>
+              </TwitterShareButton>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <LinkedinShareButton
+                url={this.props.location}
+                title={this.props.description}
+                windowWidth={750}
+                windowHeight={600}
+                className="share-modal-share">
+                <Icon
+                  name="linkedin"
+                  size="huge"
+                  className="share-linkedin-icon" />
+                  <p>LinkedIn</p>
+              </LinkedinShareButton>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <EmailShareButton
+                subject={this.props.title}
+                body={this.props.description}
+                url={this.props.location}
+                className="share-modal-share">
+                <Icon
+                  name="mail"
+                  size="huge" />
+                  <p>Email</p>
+              </EmailShareButton>
+            </Grid.Column>
+          </Grid.Row>
         </Grid>
+          <Grid columns="equal">
+              <Grid.Column></Grid.Column>
+              <Grid.Column width={14}>
+                <Input size='small' ref={this.locationRef} defaultValue={this.props.location} className="share-text-input" />
+                {this.state.textHasBeenCopied}
+                <Button content={this.state.copyButtonText} size={'small'} onClick={this.handleCopy} className="share-copy-button" />
+              </Grid.Column>
+              <Grid.Column></Grid.Column>
+          </Grid>
+        </Modal.Content>
       </Modal>
   }
 }
