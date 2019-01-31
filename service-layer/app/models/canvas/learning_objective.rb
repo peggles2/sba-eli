aac2401db688701dadc69bb1d0f5553a6bc6e437
Matthew Castle <matthew.ray.castle@gmail.com>
2019-01-31 05:48:33 -0800

    Switched learning_objective to use new paginated masquerade method

--- service-layer/app/models/canvas/learning_objective.rb
+++ service-layer/app/models/canvas/learning_objective.rb
@@ -6,7 +6,7 @@ module Canvas
     base_uri ENV["CANVAS_HOST"] + "/api/v1"
 
     def self.all(course_id)
-      uri = masquerade_current_user("/courses/#{course_id}/modules?per_page=100")
+      uri = masquerade_current_user_paginated("/courses/#{course_id}/modules")
       result = JSON.parse get(uri, base_options).body
       result.each do |topic|
         custom_data = TopicCustomData.where(topic_id: topic["id"]).first
