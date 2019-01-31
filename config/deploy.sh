28e829815597ef32fb7101d64ba65330035082f3
tjfamodu <tjfamodu@gmail.com>
2019-01-31 10:05:27 -0500

    obtain credentials via ssm parameters

--- config/deploy.sh
+++ config/deploy.sh
@@ -1,65 +1,45 @@
 #!/usr/bin/env bash
-set -eo pipefail
+set -ex
 
-export TAG=$(echo $CIRCLE_SHA1 | head -c 8)
-export BRANCH=$(echo $CIRCLE_BRANCH | sed -r 's/[_]+/-/g')
-export DATE=$(date '+%Y-%m-%d')
+echo 'export TAG=$(echo $CIRCLE_SHA1 | head -c 8)'  >> $BASH_ENV
+echo 'export BRANCH=$(echo $CIRCLE_BRANCH | sed -r 's/[_]+/-/g')' >> $BASH_ENV
+echo 'export DATE=$(date '+%Y-%m-%d')' >> $BASH_ENV
+
+source $BASH_ENV
 
 #Create Cluster
-aws ecs create-cluster --cluster "$BRANCH"
+aws ecs create-cluster --cluster "${BRANCH}"
 
-#Create Load Balancer
-ELB_ARN=$(aws elbv2 create-load-balancer --name $BRANCH --subnets $PUBLIC_SUBNET_ONE \
-  $PUBLIC_SUBNET_TWO --security-groups $LB_SECURITY_GROUP --scheme internet-facing \
-  --type application | jq --raw-output --exit-status '.LoadBalancers[].LoadBalancerArn')
-
-export ELB_DNS=$(aws elbv2 describe-load-balancers --load-balancer-arns $ELB_ARN | \
-  jq  --raw-output --exit-status '.LoadBalancers[].DNSName')
-echo "$ELB_DNS"
-
-# Create Target Group
-if TARGET_GROUP_ARN=$(aws elbv2 create-target-group --name $BRANCH --protocol HTTP \
-  --port 80 --vpc-id $VPC --target-type ip | jq --raw-output --exit-status '.TargetGroups[].TargetGroupArn'); then
-  echo "$TARGET_GROUP_ARN"
-else
-  echo "Failed to create target group"
-fi
-
-# Create Listener
-if LISTENER_ARN=$(aws elbv2 create-listener --load-balancer-arn $ELB_ARN --protocol HTTP \
-  --port 80 --default-actions Type=forward,TargetGroupArn=$TARGET_GROUP_ARN); then
-  echo "$LISTENER_ARN"
-else
-  echo "Failed to create listener"
-fi
-
-# Create Service
 ecs-cli compose --project-name $BRANCH --ecs-params config/ecs-params.yml \
   --file docker-compose-aws.yml service up --launch-type FARGATE --create-log-groups \
-  --cluster $BRANCH --container-port 80 --container-name nginx \
-  --health-check-grace-period 300 --target-group-arn $TARGET_GROUP_ARN --timeout 15
+  --cluster $BRANCH --timeout 15
+
+ECS_IP=$(ecs-cli ps --cluster $BRANCH | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -n 1)
+echo $ECS_IP
 
 # Create DNS Record
-# cat > change-batch.json << EOF
-# {
-#   "Comment": "change batch request on ${DATE}",
-#   "Changes": [
-#     {
-#       "Action": "CREATE",
-#       "ResourceRecordSet": {
-#         "Name": "${BRANCH}.${DOMAIN}",
-#         "Type": "A",
-#         "AliasTarget": {
-#           "HostedZoneId": "${HOSTED_ZONE_ID}",
-#           "DNSName": "${ELB_DNS}",
-#           "EvaluateTargetHealth": true
-#         }
-#       }
-#     }
-#   ]
-# }
-# EOF
+cat > change-batch.json << EOF
+ {
+   "Comment": "change batch request on ${DATE}",
+   "Changes": [
+     {
+       "Action": "CREATE",
+       "ResourceRecordSet": {
+         "Name": "${BRANCH}.${DOMAIN}",
+         "Type": "A",
+         "TTL": 60,
+         "ResourceRecords": [
+           {
+             "Value":"${ECS_IP}"
+           }
+         ]
+       }
+     }
+   ]
+ }
+EOF
 
-# aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --change-batch file://change-batch.json
+aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} \
+  --change-batch file://change-batch.json
 
-printf "Done, application is at: ${ELB_DNS}";
+sleep 60
--- config/destroy.sh
+++ config/destroy.sh
@@ -2,28 +2,29 @@
 set -eo pipefail
 
 # Delete DNS Record
-# cat > change-batch.json << EOF
-# {
-#   "Comment": "change batch request on ${DATE}",
-#   "Changes": [
-#     {
-#       "Action": "DELETE",
-#       "ResourceRecordSet": {
-#         "Name": "${BRANCH}.${DOMAIN}",
-#         "Type": "A",
-#         "AliasTarget": {
-#           "HostedZoneId": "${HOSTED_ZONE_ID}",
-#           "DNSName": "${ELB_DNS}",
-#           "EvaluateTargetHealth": true
-#         }
-#       }
-#     }
-#   ]
-# }
-# EOF
+cat > change-batch.json << EOF
+{
+  "Comment": "change batch request on ${DATE}",
+  "Changes": [
+    {
+      "Action": "DELETE",
+      "ResourceRecordSet": {
+        "Name": "${BRANCH}.${DOMAIN}",
+        "Type": "CNAME",
+        "TTL": 60,
+        "ResourceRecords": [
+          {
+            "Value":"${ELB_DNS}"
+          }
+        ]
+      }
+    }
+  ]
+}
+EOF
 
 # echo "Deleting DNS Record set..."
-# aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --change-batch file://change-batch.json
+aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --change-batch file://change-batch.json
 
 echo "Destroying Fargate Service..."
 ecs-cli compose --file docker-compose-aws.yml --project-name ${BRANCH} service rm --cluster ${BRANCH}
