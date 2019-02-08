e5ed8cae3f9e68286db3733207b14b5343ca7d78
Alex Bradley <alex.robert.bradley@gmail.com>
2019-02-08 13:25:51 -0500

    Pull Master from Origin

--- service-layer/app/models/learning_objective.rb
+++ service-layer/app/models/learning_objective.rb
@@ -2,7 +2,7 @@
 #
 # Table name: context_modules
 #
-#  id                          :bigint(8)        not null
+#  id                          :bigint(8)        not null, primary key
 #  context_id                  :bigint(8)        not null
 #  context_type                :string(255)      not null
 #  name                        :text
@@ -19,6 +19,14 @@
 #  cloned_item_id              :bigint(8)
 #  completion_events           :text
 #  requirement_count           :integer
+#
+# Indexes
+#
+#  index_context_modules_on_context_id_and_context_type  (context_id,context_type)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (cloned_item_id => cloned_items.id)
 #
 
 class LearningObjective < ApplicationRecord
