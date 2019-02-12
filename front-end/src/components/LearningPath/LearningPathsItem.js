a015d7b2c527e128c7646fde5ea79e47af22cec2
Kelly <kdyer@users.noreply.github.com>
2019-02-12 10:33:39 -0500

    Fix unreachable code warning in learning path item

--- front-end/src/components/LearningPath/LearningPathsItem.js
+++ front-end/src/components/LearningPath/LearningPathsItem.js
@@ -16,7 +16,7 @@ export class LearningPathsItem extends Component {
       return completed;
     } else {
       return completed;
-    };
+    } 
   };
 
   totalMicroLearningEvents = () => {
@@ -29,7 +29,7 @@ export class LearningPathsItem extends Component {
       return total;
     } else {
       return total;
-    };
+    }
   };
 
   learningPathProgress = () => {
