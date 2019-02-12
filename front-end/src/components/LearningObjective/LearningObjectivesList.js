0b06bf2b1e122afd67f386c2d3ecdcdffe28d5d0
Matt Castle <matthew.ray.castle@gmail.com>
2019-02-12 13:43:08 -0500

    Added support to use objectives collection in store
    Modified the way the objectives for admin calls were saved in store so
    accounted for this where it was called in components

--- front-end/src/components/LearningObjective/LearningObjectivesList.js
+++ front-end/src/components/LearningObjective/LearningObjectivesList.js
@@ -3,7 +3,7 @@ import { List } from "semantic-ui-react";
 import { connect } from "react-redux";
 
 import LearningObjectivesItem from "./LearningObjectivesItem";
-import { getLearningObjectivesForAdmin } from '../../actions/learningObjectiveActions';
+import { getObjectivesForAdminIfNeeded } from "../../actions/learningObjectiveActions";
 
 export class LearningObjectivesList extends Component {
   constructor(props) {
@@ -19,26 +19,37 @@ export class LearningObjectivesList extends Component {
   }
 
   objectivesList() {
-    this.props.dispatch(getLearningObjectivesForAdmin(this.props.course_id));
+    this.props.dispatch(getObjectivesForAdminIfNeeded(this.props.course_id));
   }
 
   renderObjectivesList(objectives = []) {
     if (objectives.length) {
       return objectives.map(objective => {
-        return <LearningObjectivesItem key={objective.id} course_id={this.props.course_id} item={objective} />;
+        return (
+          <LearningObjectivesItem
+            key={objective.id}
+            course_id={this.props.course_id}
+            item={objective}
+          />
+        );
       });
     }
     return <List.Item>No data</List.Item>;
   }
 
   render() {
-    return <List>{this.renderObjectivesList(this.props.learningObjectives)}</List>;
+    const { adminObjectivesCollection, course_id } = this.props;
+    return (
+      <List>
+        {this.renderObjectivesList(adminObjectivesCollection[course_id])}
+      </List>
+    );
   }
 }
 
 const mapStateToProps = store => {
   return {
-    learningObjectives: store.learningObjective.learningObjectives
+    adminObjectivesCollection: store.learningObjective.adminObjectivesCollection
   };
 };
 
--- front-end/src/components/Navigation/NavigationLearningObjective.js
+++ front-end/src/components/Navigation/NavigationLearningObjective.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { Divider, List } from "semantic-ui-react";
 import { connect } from "react-redux";
 
-import { getLearningObjectivesForAdmin } from "../../actions/learningObjectiveActions";
+import { getObjectivesForAdminIfNeeded } from "../../actions/learningObjectiveActions";
 
 export class NavigationLearningObjective extends Component {
   componentDidMount() {
@@ -12,12 +12,12 @@ export class NavigationLearningObjective extends Component {
 
   fetchData() {
     this.props.dispatch(
-      getLearningObjectivesForAdmin(this.props.learningPathId)
+      getObjectivesForAdminIfNeeded(this.props.learningPathId)
     );
   }
 
   topicNumber() {
-    const objectives = this.props.learningObjectives || [];
+    const objectives = this.getObjectivesFromCollection();
     const topicNumber = objectives.length;
 
     if (topicNumber === 1) {
@@ -39,9 +39,14 @@ export class NavigationLearningObjective extends Component {
     );
   }
 
+  getObjectivesFromCollection() {
+    const { adminObjectivesCollection, learningPathId: pathId } = this.props;
+    return adminObjectivesCollection[pathId] || [];
+  }
+
   render() {
-    const pathId = this.props.learningPathId;
-    const objectives = this.props.learningObjectives || [];
+    const { learningPathId: pathId } = this.props;
+    const objectives = this.getObjectivesFromCollection();
     const topics = objectives.map((lo, index) => {
       return (
         <List.Item key={"learning_objective_" + index}>
@@ -71,7 +76,7 @@ export class NavigationLearningObjective extends Component {
 
 const mapStateToProps = store => {
   return {
-    learningObjectives: store.learningObjective.learningObjectives
+    adminObjectivesCollection: store.learningObjective.adminObjectivesCollection
   };
 };
 
