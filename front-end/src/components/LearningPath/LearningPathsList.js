a1244dba883d281559228fd63122135a5c572df2
Kelly <kdyer@users.noreply.github.com>
2019-02-12 11:33:32 -0500

    Update the import on Learning Paths list to align with the code base

--- front-end/src/components/LearningPath/LearningPathsList.js
+++ front-end/src/components/LearningPath/LearningPathsList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import { Card } from "semantic-ui-react";
 import { connect } from "react-redux";
 import LearningPathsItem from "./LearningPathsItem";
@@ -6,7 +6,7 @@ import './LearningPathsList.scss';
 
 import { getLearningPaths, getLearningPathsProgress } from "../../actions/learningPathActions";
 
-export class LearningPathsList extends React.Component {
+export class LearningPathsList extends Component {
   componentDidMount() {
     this.props.getLearningPaths();
     this.props.getLearningPathsProgress();
